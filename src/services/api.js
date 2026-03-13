4a5c30823bc1c1a58cfa9c7c7a177c7c8008c595
mpmabeyrathne <pasindumaduwantha2019@gmail.com>
2026-03-14 01:36:52 +0530

    add elastic ip

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/items';
+const API_URL = 'http://13.206.33.87:5000/api/items';
 
 export const fetchItems = async () => {
     const res = await fetch(API_URL);
